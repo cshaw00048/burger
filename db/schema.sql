5148a7dbd23faac2b037da514ce885d88965de3b
Christian Shaw <cshaw00048@gmail.com>
2020-05-11 06:23:43 -0500

    created databases (schema.sql and seeds.sql)

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers(
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    burger_name varchar(100) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+)
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Brian Gary Burger");
+INSERT INTO burgers (burger_name) VALUES ("Rodeo Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Tennessee Burger", true);
